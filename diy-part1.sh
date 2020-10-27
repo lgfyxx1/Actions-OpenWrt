9022b0872546728c7e941a377704fb0251c59640
lgfyxx1 <68235325+lgfyxx1@users.noreply.github.com>
2020-10-27 16:11:54 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -11,10 +11,10 @@
 # Uncomment a feed source
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 # 移除不用软件包
-rm -rf package/lienol/luci-app-passwall
+# rm -rf package/lienol/luci-app-passwall
 # rm -rf package/lean/luci-app-ssr-plus
 # 添加软件包
-sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-package.git' feeds.conf.default
+# sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-package.git' feeds.conf.default
 # sed -i '$a src-git lienol https://github.com/lgfyxx/lienol-openwrt-package.git' feeds.conf.default
 # sed -i '$a src-git ssr https://github.com/fw876/helloworld.git' feeds.conf.default
 
