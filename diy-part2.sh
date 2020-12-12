8fe4ae147136a17300c5eb027c7e3b33b23ab125
lgfyxx1 <68235325+lgfyxx1@users.noreply.github.com>
2020-12-12 23:06:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,3 +13,6 @@ sed -i 's/192.168.1.1/192.168.123.99/g' package/base-files/files/bin/config_gene
 # 内核切换
 # sed -i 's/KERNEL_PATCHVER:=5.4/KERNEL_PATCHVER:=4.14/g' ./target/linux/ramips/Makefile
 # sed -i 's/KERNEL_TESTING_PATCHVER:=5.4/KERNEL_TESTING_PATCHVER:=4.14/g' ./target/linux/ramips/Makefile
+# 移除不用软件包
+rm -rf package/lienol/luci-app-passwall
+# rm -rf package/lean/luci-app-ssr-plus
